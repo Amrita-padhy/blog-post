9213ace02e173b8e37bc973233df315a34118f20
bibhu-padhy <bibhupadhy140@gmail.com>
2022-05-24 17:03:52 +0530

    add firebase

---
+++ UI/src/util/firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyBrHC_yghDNy7TptRJ_ADD6bL8A2FbvAb8",
+  authDomain: "react-demo-e1d88.firebaseapp.com",
+  projectId: "react-demo-e1d88",
+  storageBucket: "react-demo-e1d88.appspot.com",
+  messagingSenderId: "794527527422",
+  appId: "1:794527527422:web:9ed8e896ff6f385d9e90af",
+  measurementId: "G-BC6ZMW3TQL",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export const login = async (data) => {
+  const { user } = await signInWithEmailAndPassword(
+    auth,
+    data.email,
+    data.password
+  );
+  console.log(user);
+  if (user.uid) {
+    return true;
+  } else {
+    return false;
+  }
+};
